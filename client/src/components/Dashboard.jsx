dce18e517d3f2e8bbc6f10443560e4a9ccb848a1
unknown <thecoderjangir@gmail.com>
2022-07-24 12:45:05 +0530

    make it better

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -33,7 +33,7 @@ const Dashboard = () => {
     const fetchApiData = async () => {
         try {
             const token = localStorage.getItem('token')
-            const resp = await fetch(`${process.env.REACT_APP_API_KEY}/api/addnotes`, {
+            const resp = await fetch(`${process.env.REACT_APP_API_KEY}/addnotes`, {
                 method: "GET",
                 headers: {
                     "Content-Type": "application/json",
@@ -52,13 +52,13 @@ const Dashboard = () => {
         try {
             const date = todayDate.format(now, 'hh:mm A MMM DD YYYY');
             const token = localStorage.getItem('token')
-            const resp = await fetch(`${process.env.REACT_APP_API_KEY}/api/addnotes`, {
+            const resp = await fetch(`${process.env.REACT_APP_API_KEY}/addnotes`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
                     "token": token
                 },
-
+                
                 body: JSON.stringify({ title, description, date })
             })
             const data = await resp.json()
@@ -76,7 +76,7 @@ const Dashboard = () => {
     const deleteNote = async (id) => {
         try {
             const token = localStorage.getItem('token')
-            const resp = await fetch(`${process.env.REACT_APP_API_KEY}/api/deletenote/${id}`, {
+            const resp = await fetch(`${process.env.REACT_APP_API_KEY}/deletenote/${id}`, {
                 method: "DELETE",
                 headers: {
                     "token": token
